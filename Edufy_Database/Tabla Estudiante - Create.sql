a06762a4735e9b867de9c41a4a476fbca5a8e821
Joseph1799 <Joseph1799us@gmail.com>
2024-02-25 19:33:15 -0600

    Edufy database scripts

---
+++ Edufy_Database/Tabla Estudiante - Create.sql
@@ -0,0 +1,30 @@
+USE [edufy_db]
+GO
+
+/****** Object:  Table [dbo].[Estudiante]    Script Date: 2/25/2024 7:30:16 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Estudiante](
+	[ID] [int] NOT NULL,
+	[Nombre] [varchar](255) NOT NULL,
+	[Apellido] [varchar](255) NOT NULL,
+	[CorreoElectronico] [varchar](255) NOT NULL,
+	[Contrasenia] [varchar](255) NOT NULL,
+	[FechaNacimiento] [date] NOT NULL,
+	[Genero] [varchar](50) NOT NULL,
+	[Carrera] [varchar](255) NOT NULL,
+	[AnioIngreso] [int] NOT NULL,
+	[EstadoCuenta] [varchar](50) NOT NULL,
+	[FotoPerfil] [varchar](255) NULL,
+	[Rol] [varchar](15) NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Estudiante] ADD  DEFAULT ('Estudiante') FOR [Rol]
+GO
+
+
---
+++ Edufy_Database/Tabla Profesor - Create.sql
@@ -0,0 +1,30 @@
+USE [edufy_db]
+GO
+
+/****** Object:  Table [dbo].[Profesor]    Script Date: 2/25/2024 7:31:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Profesor](
+	[ID] [int] NOT NULL,
+	[Nombre] [varchar](255) NOT NULL,
+	[Apellido] [varchar](255) NOT NULL,
+	[CorreoElectronico] [varchar](255) NOT NULL,
+	[Contrasenia] [varchar](255) NOT NULL,
+	[FechaNacimiento] [date] NOT NULL,
+	[Genero] [varchar](50) NOT NULL,
+	[Departamento] [varchar](255) NOT NULL,
+	[TituloAcademico] [varchar](255) NOT NULL,
+	[EstadoCuenta] [varchar](50) NOT NULL,
+	[FotoPerfil] [varchar](255) NULL,
+	[Rol] [varchar](15) NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Profesor] ADD  DEFAULT ('Profesor') FOR [Rol]
+GO
+
+
